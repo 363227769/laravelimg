35084caf82a8d7630b08d4b635946a8a94506cfa
守望者520 <363227769@qq.com>
2021-05-12 17:41:14 +0800

    上传1

---
+++ tests/UploadFileTest.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the guanguans/uploadfile.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Mhz\Tests;
+
+use Mhz\UploadFile;
+use PHPUnit\Framework\TestCase;
+
+class UploadFileTest extends TestCase
+{
+    protected $UploadFile;
+
+    public function setUp()
+    {
+        $this->UploadFile = new UploadFile();
+    }
+
+    public function testGetErrorMsg()
+    {
+        $this->assertNotEmpty('this is a test');
+    }
+}
